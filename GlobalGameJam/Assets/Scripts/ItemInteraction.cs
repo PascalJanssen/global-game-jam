3731c34dd32b7b7de699311e1240bcdfaa2e3dbe
PascalJanssen <pascaljanssen96@gmail.com>
2019-01-25 21:44:22 +0100

    CharacterController

---
+++ GlobalGameJam/Assets/Scripts/ItemInteraction.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInteraction : MonoBehaviour
+{
+
+    public GameObject GrabUI;
+    public Transform ItemHoldPlace;
+    private GameObject ItemHold;
+    public Transform DropItem;
+    private bool itemIsHolding = false;
+
+    private void Update()
+    {
+        if (itemIsHolding)
+        {
+            if (Input.GetButtonUp("action"))
+            {
+                ItemHold.transform.SetPositionAndRotation(DropItem.position, DropItem.rotation);
+                itemIsHolding = false;
+            }
+            else
+            {
+                ItemHold.transform.SetPositionAndRotation(ItemHoldPlace.position, ItemHoldPlace.rotation);
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("item"))
+        {
+            if (Input.GetButtonDown("action"))
+            {
+                ItemHold = other.gameObject;
+                ItemHold.transform.SetPositionAndRotation(ItemHoldPlace.position, ItemHoldPlace.rotation);
+                itemIsHolding = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("item"))
+        {
+            GrabUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("item"))
+        {
+            GrabUI.SetActive(false);
+        }
+    }
+}
---
+++ GlobalGameJam/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+
+    public float movementSpeed;
+    public float rotationSpeed;
+
+    private Rigidbody rigidbody;
+    private Vector3 groundMovement;
+    private Vector3 rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = transform.forward * Input.GetAxis("Vertical");
+
+        float xRotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        groundMovement = move * movementSpeed;
+
+        rotation = new Vector3(0, xRotation, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = groundMovement * Time.fixedDeltaTime;
+        transform.Rotate(rotation, Space.Self);
+    }
+}
