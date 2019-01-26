24aa019380fc113089130a503f48ce3dbd19b2f8
PascalJanssen <pascaljanssen96@gmail.com>
2019-01-26 01:05:58 +0100

    trying to change gravity

--- GlobalGameJam/Assets/Scripts/PlayerMovementController.cs
+++ GlobalGameJam/Assets/Scripts/PlayerMovementController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,14 +9,32 @@ public class PlayerMovementController : MonoBehaviour
     public float movementSpeed;
     public float rotationSpeed;
 
+    public Transform PlayerModel;
+
     private Rigidbody rigidbody;
     private Vector3 groundMovement;
     private Vector3 rotation;
+    private Vector3 gravityDirection = new Vector3(0,-9.81f,0);
+    private ConstantForce gravity;
+
+    private bool isWalkingOnWall = false;
+    private bool suctionCup = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        gravity = gameObject.AddComponent<ConstantForce>();
+    }
+
+    public void WallIsWalkable(Collider other)
+    {
+        if (suctionCup)
+        {
+            rigidbody.useGravity = false;
+            gravityDirection = PlayerModel.forward * 9.81f;
+            PlayerModel.Rotate(new Vector3(90, 0, 0), Space.Self);
+        }
     }
 
     // Update is called once per frame
@@ -28,10 +47,20 @@ public class PlayerMovementController : MonoBehaviour
         groundMovement = move * movementSpeed;
 
         rotation = new Vector3(0, xRotation, 0);
+
+        if (Input.GetButtonDown("suction") && !suctionCup)
+        {
+            suctionCup = true;
+        }
+        else if (Input.GetButtonDown("suction") && suctionCup)
+        {
+            suctionCup = false;
+        }
     }
 
     private void FixedUpdate()
     {
+        gravity.force = gravityDirection;
         rigidbody.velocity = groundMovement * Time.fixedDeltaTime;
         transform.Rotate(rotation, Space.Self);
     }
---
+++ GlobalGameJam/Assets/Scripts/WalkableWall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkableWall : MonoBehaviour
+{
+
+    public PlayerMovementController controller;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("walkable"))
+        {
+            controller.WallIsWalkable(other);
+        }
+    }
+}
